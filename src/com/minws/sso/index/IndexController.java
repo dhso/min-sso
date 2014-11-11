66dc673ca56b67c367284e3daf57381bdd4efffa
dhso <dhso@163.com>
2014-11-11 23:36:02 +0800

    init

---
+++ src/com/minws/sso/index/IndexController.java
@@ -0,0 +1,17 @@
+package com.minws.sso.index;
+
+import com.jfinal.core.Controller;
+
+/**
+ * IndexController
+ */
+public class IndexController extends Controller {
+	public void index() {
+		render("index.jsp");
+	}
+}
+
+
+
+
+
