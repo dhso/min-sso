8d4c81237aa13adb395d600cdf4107b64b30a2f2
dhso <dhso@163.com>
2014-11-12 22:50:15 +0800

    init

--- src/main/java/com/minws/sso/config/AppConfig.java
+++ src/main/java/com/minws/sso/config/AppConfig.java
@@ -7,8 +7,6 @@ import com.jfinal.config.JFinalConfig;
 import com.jfinal.config.Plugins;
 import com.jfinal.config.Routes;
 import com.jfinal.core.JFinal;
-import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
-import com.jfinal.plugin.c3p0.C3p0Plugin;
 import com.jfinal.render.ViewType;
 import com.minws.sso.auth.AuthController;
 import com.minws.sso.frame.util.ProsMap;
@@ -37,13 +35,6 @@ public class AppConfig extends JFinalConfig {
 	 * 配置插件
 	 */
 	public void configPlugin(Plugins me) {
-		// 配置C3p0数据库连接池插件
-		//C3p0Plugin c3p0Plugin = new C3p0Plugin(ProsMap.getStrPro("sso.jdbcUrl"), ProsMap.getStrPro("sso.user"), ProsMap.getStrPro("sso.password").trim());
-		//me.add(c3p0Plugin);
-		
-		// 配置ActiveRecord插件
-		//ActiveRecordPlugin arp = new ActiveRecordPlugin(c3p0Plugin);
-		//me.add(arp);
 		
 	}
 	
